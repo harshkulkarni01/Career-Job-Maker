89ef812995e078f7d264d89a66d8db05f21beba6
harshkulkarni01 <hstechnical225@gmail.com>
2024-09-10 21:51:47 +0530

    Harsh

---
+++ app/src/main/java/com/hksoftwares/careerjobmaker/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.hksoftwares.careerjobmaker
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val firstFragment=Courses()
+        val secondFragment=Jobs()
+        val thirdFragment= Profile()
+
+        setCurrentFragment(firstFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.Courses->setCurrentFragment(firstFragment)
+                R.id.Jobs->setCurrentFragment(secondFragment)
+                R.id.Profile->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+}
---
+++ app/src/main/java/com/hksoftwares/careerjobmaker/Profile.kt
@@ -0,0 +1,25 @@
+package com.hksoftwares.careerjobmaker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class Profile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        // Sample data
+
+
+        return view
+    }
+}
